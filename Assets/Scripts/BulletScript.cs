67ab0dc5f7f70b1e1af7c5e0620333138c256b09
Jacob Letko <s179061@students.aie.edu.au>
2018-05-08 16:54:02 -0700

    bullet stuff

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletScript : MonoBehaviour {
+public class BulletScript : MonoBehaviour
+{
 
     public string target;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == target)
+        {
+            other.gameObject.GetComponent<GridItem>().OnBulletCollision();
+            Destroy(this.gameObject);
+        }
+    }
 }
