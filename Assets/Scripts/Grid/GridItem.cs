95c222f925d57558a598fbbd94159719f9a1cbff
Paul Gibbons <s179054@students.aie.edu.au>
2018-05-07 18:10:18 -0700

    added build grid

---
+++ Assets/Scripts/Grid/GridItem.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GridItem : MonoBehaviour
+{
+    [SerializeField]
+    private GridSys grid;
+    private MeshRenderer myMesh;
+    [SerializeField]
+    private ShipBlockList playerShip;
+
+    public int hashCode;
+
+    bool isPartOfShip;
+    // Use this for initialization
+    void Awake()
+    {
+        if (grid == null)
+        {
+            grid = FindObjectOfType<GridSys>();
+        }
+        if (playerShip == null)
+        {
+            playerShip = FindObjectOfType<ShipBlockList>();
+        }
+
+        myMesh = gameObject.GetComponent<MeshRenderer>();
+        myMesh.enabled = false;
+        isPartOfShip = false;
+        hashCode = GetHashCode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        if (!isPartOfShip)
+        {
+            myMesh.enabled = true;
+        }
+
+    }
+    private void OnMouseExit()
+    {
+        if (isPartOfShip)
+        {
+            myMesh.enabled = true;
+        }
+        else
+        {
+            myMesh.enabled = false;
+        }
+
+    }
+    private void OnMouseDown()
+    {
+        if (!isPartOfShip)
+        {
+            playerShip.listOfShipParts.Add(hashCode,this.gameObject);
+            gameObject.transform.parent = playerShip.transform;
+            isPartOfShip = true;
+            myMesh.enabled = true;
+        }
+        else
+        {
+            playerShip.listOfShipParts.Remove(hashCode);
+            gameObject.transform.parent = playerShip.transform;
+            isPartOfShip = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/ShipBlockList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBlockList : MonoBehaviour
+{
+    public Dictionary<int,GameObject> listOfShipParts;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
