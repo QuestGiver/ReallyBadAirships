216f343949bd90cd4b9e655b8f337327fef6bbd4
Jacob Letko <s179061@students.aie.edu.au>
2018-05-08 10:03:48 -0700

    bullets?

--- Assets/Jacob/GunScript.cs
+++ Assets/Jacob/GunScript.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunScript : MonoBehaviour {
+public class GunScript : MonoBehaviour
+{
+    public GameObject bullet;
+    List<GameObject> bullets;
+    public int count;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float timer;
+    public float fireRate;
+    public float speed;
+
+    public void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            bullets[i] = bullet;
+            bullets[i].gameObject.SetActive(false);
+            bullets[i].gameObject.transform.parent = this.transform;
+        }
+        timer = fireRate;
+        count = 10;
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+            timer -= Time.deltaTime;
+        if(timer < 0 && Input.GetMouseButton(0))
+        {
+            shoot();
+        }
+    }
+
+    private void shoot()
+    {
+        GameObject B = bullets[count];
+        B.SetActive(true);
+        Vector3 shootDir = (Input.mousePosition);
+        B.GetComponent<Rigidbody>().velocity = shootDir * speed;
+    }
 }
