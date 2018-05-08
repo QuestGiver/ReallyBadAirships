9681f752ced32582d874a9b4cfac8203327e277a
Jacob Letko <s179061@students.aie.edu.au>
2018-05-08 10:15:02 -0700

    unkown

---
+++ Assets/Jacob/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private PlayerMotor motor;
+
+	void Start ()
+    {
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float Xmove = Input.GetAxis("Horizontal");
+        float Zmove = Input.GetAxis("Vertical");
+
+        Vector3 moveHorizontal = transform.right * Xmove;
+        Vector3 moveVertical = transform.forward * Zmove;
+
+        Vector3 velocity = (moveHorizontal + moveVertical) * speed;
+
+        motor.Move(velocity);
+	}
+}
---
+++ Assets/Jacob/PlayerMotor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 vec)
+    {
+        velocity = vec;
+    }
+
+    private void FixedUpdate()
+    {
+        performMovement();
+    }
+
+    void performMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+}
